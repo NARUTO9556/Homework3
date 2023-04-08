ba11eec17ccfa19666abf9420dec5820b0f3608d
NARUTO9556 <gruver5263@gmail.com>
2023-04-08 15:56:15 +0800

    1-2 задача

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,33 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        byte b = 4;
+        short s = 32;
+        int i = 525;
+        long l = 360L;
+        float f = 62.35f;
+        double d = 65.44;
+        System.out.println("Значение переменной b с типом byte равно " + b );
+        System.out.println("Значение переменной s с типом short равно " + s );
+        System.out.println("Значение переменной i с типом int равно " + i);
+        System.out.println("Значение переменной l с типом long равно " + l );
+        System.out.println("Значение переменной f с типом float равно " + f );
+        System.out.println("Значение переменной d с типом double равно " + d );
 
-        }
+        float weigth = 27.12f;
+        long moneyCase = 987678965549L;
+        double heigth = 2.786;
+        short money = 569;
+        short balanceBankCard = -159;
+        int price = 27897;
+        byte pence = 67;
+        System.out.println(weigth);
+        System.out.println(moneyCase);
+        System.out.println(heigth);
+        System.out.println(money);
+        System.out.println(balanceBankCard);
+        System.out.println(price);
+        System.out.println(pence);
+
     }
+}
